ee43624beafa3883620c77c0ee442449c1d07a48
FrancisXiaobu <vampcheah@gmail.com>
2026-07-31 14:58:44 +0800

    fix: offset notification badge number down by 1px for optical alignment

--- src/lib/shell/NotificationsMenu.svelte
+++ src/lib/shell/NotificationsMenu.svelte
@@ -40,7 +40,7 @@
 					<Badge
 						class="absolute -top-1 -right-1 flex h-4 min-w-4 items-center justify-center rounded-full p-0 px-1 text-[10px] font-semibold leading-none tabular-nums"
 					>
-						{unread > 9 ? '9+' : unread}
+						<span class="translate-y-px">{unread > 9 ? '9+' : unread}</span>
 					</Badge>
 				{/if}
 			</Button>
